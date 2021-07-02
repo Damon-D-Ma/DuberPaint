671173e828c6013f1109e0d6b4578bfa21515735
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-07-02 12:08:41 -0400

    um does this count as code?

---
+++ server.py
@@ -0,0 +1,17 @@
+from threading import Thread
+
+def main():
+    """
+    The main function
+    """
+    pass
+
+class ClientListener:
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+    main()
