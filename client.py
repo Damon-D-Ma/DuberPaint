b5cf930c070073d1ca2d61fd9b2fb3a661de99ee
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-07-03 10:41:30 -0400

    connecting works!!!

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import pygame
+import socket
 
 def main():
-    pass
+    """
+    The main function
+    """
+    ip = input("enter ip")
+    port = int(input("enter port: "))
+    socket.create_connection((ip, port))
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 from threading import Thread
+import socket
+        
+threads = []
+
+def client_listener(conn, addr):
+    pass
 
 def main():
     """
     The main function
     """
-    pass
-
-class ClientListener:
-    def __init__(self):
-        pass
-    
-    def run(self):
-        pass
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.bind(("127.0.0.1", 5000))
+        sock.listen()
+        conn, addr = sock.accept()
+        thread = Thread(target=client_listener, args=(conn, addr))
+        thread.start()
 
 if __name__ == "__main__":
     main()
