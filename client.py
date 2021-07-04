7a877e50f4985a4e1e76821170d557c066fd8c2e
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-07-04 11:51:47 -0400

    surprisingly nothing has broken so far

--- client.py
+++ client.py
@@ -1,13 +1,28 @@
+from threading import Thread
 import pygame
 import socket
+
+def server_interaction (ip, port):
+    """
+    Interacts with the server (listens and sends)
+
+    Args:
+        ip (string): The IP to connect to
+        port (int): The port to connect to on that specific IP
+    """
+    sock = socket.create_connection((ip, port))
+    while 1:
+        data = input("send something: ")
+        sock.send(data.encode())
 
 def main():
     """
     The main function
     """
-    ip = input("enter ip")
+    ip = input("enter ip: ")
     port = int(input("enter port: "))
-    socket.create_connection((ip, port))
+    thread = Thread(target=server_interaction, args=(ip, port))
+    thread.start()
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -4,18 +4,29 @@ import socket
 threads = []
 
 def client_listener(conn, addr):
-    pass
+    """
+    Listens to a specific client
+
+    Args:
+        conn (socket.socket object): socket to listen to
+        addr (tuple): Where the connection is coming from
+    """
+    print(f"connection from {addr}")
+    while 1:
+        data = conn.recv(4096).decode("utf-8")
+        print(data)
 
 def main():
     """
     The main function
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind(("127.0.0.1", 5000))
-        sock.listen()
-        conn, addr = sock.accept()
-        thread = Thread(target=client_listener, args=(conn, addr))
-        thread.start()
+    while 1:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind(("127.0.0.1", 5000))
+            sock.listen()
+            conn, addr = sock.accept()
+            thread = Thread(target=client_listener, args=(conn, addr))
+            thread.start()
 
 if __name__ == "__main__":
     main()
