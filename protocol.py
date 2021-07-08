3d09882dd69770cf8be1cf2f5a95d03dea77268f
Damon Ma <damondma@gmail.com>
2021-07-08 19:28:47 -0400

    Create protocol.py

---
+++ protocol.py
@@ -0,0 +1,29 @@
+"""
+Contains all needed protocol for the drawing program
+"""
+class protocol:
+    """
+    class for general protocol between the server and client programs
+    """
+    def __init__():
+    """
+    empty constructor for the class
+    """
+
+    def encode(message):
+        """
+        Encodes a received message
+        """
+    def decode(message):
+       """
+       Decodes a received message
+       """
+    def send():
+        """
+        Sends a message
+        """
+    def recv():
+        """
+        Receives a message
+        """
+
