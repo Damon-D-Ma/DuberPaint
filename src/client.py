0b8f696106aaa6a94f3caa9f8989ad41730d194a
Damon Ma <damondma@gmail.com>
2021-07-28 09:35:08 -0400

    Update client.py

--- src/client.py
+++ src/client.py
@@ -20,10 +20,29 @@ def main():
     """
     The main function
     """
+
+    #Initialize pygame
+    pygame.init()
+
+    window = pygame.display.set_mode((1080, 720))
+
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        
+    
+   
+    
+    """
+    text-based login code to be scrapped later
+
     ip = input("enter ip: ")
     port = int(input("enter port: "))
     thread = Thread(target=server_interaction, args=(ip, port))
     thread.start()
-
+    """
 if __name__ == "__main__":
     main()
